0c35fb59faffca43a9e1f78a98ecfe9481503bbe
Zack Polizzi <zplizzi@gmail.com>
2016-08-01 15:06:11 -0700

    Fixed the fuck out of #2!

--- setup/setup_lite.sh
+++ setup/setup_lite.sh
@@ -1,7 +1,9 @@
 # This is intended to setup only minimal things on systems with arbitrary OS's
+echo "You need git and zsh installed prior to running this script!"
 
-
+git config --global push.default matching
 bash ~/zeus/setup/setup_vim.sh
-bash ~/zeus/setup/setup_zsh.sh
-
 bash ~/zeus/setup/setup.sh
+bash ~/zeus/setup/setup_zsh.sh
+git config --global push.default matching
+
