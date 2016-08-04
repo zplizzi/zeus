e90f33c668a8a2fe72d6107ac29bb3aa36065104
Zack Polizzi <zplizzi@gmail.com>
2016-08-04 04:07:16 +0000

    rats

---
+++ setup/setup_dropbox.sh
@@ -0,0 +1,12 @@
+# Donwload dropbox app to ~/.dropbox_dist
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -
+
+# Download dropbox command line tool
+cd ~/.dropbox_dist/
+wget --output-document "dropbox.py" https://www.dropbox.com/download?dl=packages/dropbox.py 
+
+# Start the app, you'll have to click the link
+python dropbox.py start
+
+# set it to autostart
+python dropbox.py autostart
