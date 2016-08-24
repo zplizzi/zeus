f491f5081319cbd18eda0df99aa6404e581276ff
Zack Polizzi <zplizzi@gmail.com>
2016-08-23 23:41:43 -0700

    uhhhhhh

--- setup/setup_jupyter.sh
+++ setup/setup_jupyter.sh
@@ -2,4 +2,4 @@ pip install jupyter
 rm -rf ~/.jupyter
 mkdir -p ~/.jupyter
 cp jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
-echo "Success"
+echo "Successfully configured jupyter notebook server."
