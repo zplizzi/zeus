a803742267e0b1529034aaaa8744b52a8d41984c
Zack Polizzi <zplizzi@gmail.com>
2016-09-18 23:30:03 -0400

    apparently i did something…

---
+++ setup/setup_ros.sh
@@ -0,0 +1,22 @@
+# Install PPA
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# Add keys
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+
+# Update packages
+sudo apt-get update
+
+# Install everything kinetic
+# WILL ONLY WORK ON 16.04 (and a few others)
+echo "Installing ROS kinetic. Will likely fail if you're not on 16.04 or similar"
+sudo apt-get install ros-kinetic-desktop-full
+
+# Update rosdep
+sudo rosdep init
+rosdep update
+
+# Add ros init to bashrc
+echo "Adding ROS init to .zshrc. If you're using bash, do this manually."
+echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
+source ~/.zshrc
