129c400d84313df77d6e686333cbbeca62d8cfa2
Zack Polizzi <zplizzi@gmail.com>
2016-11-26 06:19:53 +0000

    Refactor factories, revisit visitors

--- setup/setup_dropbox.sh
+++ setup/setup_dropbox.sh
@@ -2,7 +2,7 @@
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -
 
 # Download dropbox command line tool
-cd ~/.dropbox_dist/
+cd ~/.dropbox-dist/
 wget --output-document "dropbox.py" https://www.dropbox.com/download?dl=packages/dropbox.py 
 
 # Start the app, you'll have to click the link
