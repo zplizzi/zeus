7f2897aff937db88a74bef616c428b13c05349f4
Zack Polizzi <zplizzi@gmail.com>
2017-05-02 04:41:53 +0000

    asdf

--- setup/setup_vim.sh
+++ setup/setup_vim.sh
@@ -4,9 +4,9 @@ set -e
 
 
 # install vundle
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+if [ ! -d ~/.vim/plugin/Vundle.vim ]; then
   echo "installing vundle"
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/plugin/Vundle.vim
 fi
 
 
