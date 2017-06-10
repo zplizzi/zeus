6ec6d075a879404ed5af8d794c194d2351cb5a36
Zack Polizzi <zplizzi@gmail.com>
2017-06-09 23:03:59 -0700

    no message specified

--- setup/setup_vim.sh
+++ setup/setup_vim.sh
@@ -14,4 +14,8 @@ fi
 echo "Installing vim plugins. Will throw an error, don't be alarmed, just hit ENTER."
 vim +PluginInstall +qall
 
-
+# Install GPG plugin by copying file to plugin dir. 
+# Need to update this file manually for new versions.
+mkdir -p ~/.vim/plugin
+cp gpg/gnupg.vim ~/.vim/plugin/gnupg.vim
+# After this, it should "just work", for symmetric-key encrypted files at least.
